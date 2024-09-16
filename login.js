714821d470c434c16b964bcdf1bdb8a40f8ef528
RafaelCelso <rafaelcelsoo@gmail.com>
2024-09-16 16:36:49 -0300

    Atualização geral

--- login.js
+++ login.js
@@ -1,31 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('loginForm');
 
-    loginForm.addEventListener('submit', async (e) => {
+    loginForm.addEventListener('submit', (e) => {
         e.preventDefault();
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
         
-        try {
-            const response = await fetch('http://localhost:3000/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ email, password }),
-            });
-
-            const data = await response.json();
-
-            if (response.ok) {
-                localStorage.setItem('currentUser', JSON.stringify(data.user));
-                window.location.href = 'index.html';
-            } else {
-                alert(`Erro: ${data.error}`);
-            }
-        } catch (error) {
-            console.error('Erro:', error);
-            alert('Ocorreu um erro ao fazer login. Por favor, tente novamente.');
+        const users = JSON.parse(localStorage.getItem('users')) || [];
+        const user = users.find(u => u.email === email && u.password === password);
+        
+        if (user) {
+            localStorage.setItem('currentUser', JSON.stringify(user));
+            window.location.href = 'index.html';
+        } else {
+            alert('E-mail ou senha incorretos');
         }
     });
 });
